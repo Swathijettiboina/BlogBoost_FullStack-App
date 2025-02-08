baaf8e031493876ddefbc845a9dea8020ca9fd5f
Swathijetti2004 <swathijettiboina@gmail.com>
2025-02-08 20:59:15 +0530

    functionalities added and frontend designed

--- frontend/src/pages/FeaturedBlogs.jsx
+++ frontend/src/pages/FeaturedBlogs.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { ChevronLeft } from "lucide-react"; // ✅ Import Fixed
 import BlogCard from "../components/BlogCard";
 
 const FeaturedBlogs = () => {
